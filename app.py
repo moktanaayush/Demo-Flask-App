26895222695d99f28c072426fe289e87a3bb98d5
John Michael <mjnishore@gmail.com>
2025-07-04 10:41:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,4 @@ def delete_item(item_id):
 
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
